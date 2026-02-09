4a051471c8c93b8f701997033d21bf16e40df769
Emad Alkhashab <emadelkhashab@hotmail.com>
2026-02-09 12:15:24 +0200

    Add PTv3 model with training loop

---
+++ ptv3_util.py
@@ -0,0 +1,77 @@
+import numpy
+from model import *
+import h5py
+import os
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from point_transformer_pytorch import PointTransformerLayer
+
+def save_checkpoint(epoch, model, optimizer, loss, filepath):
+	checkpoint = {
+		'epoch': epoch,
+		'model_state_dict': model.state_dict(),
+		'optimizer_state_dict': optimizer.state_dict(),
+		'loss': loss,
+	}
+	torch.save(checkpoint, filepath)
+
+class RegionTransformerPTv3(nn.Module):
+	# Updated to use PointTransformerV3
+	def __init__(self, batch_size=32,
+				 seq_len=1,
+				 num_inlier_points=512,
+				 num_neighbour_points=512,
+				 feature_dim=12):
+		super(RegionTransformerPTv3, self).__init__()
+		self.num_points = num_inlier_points
+		self.batch_size = batch_size
+		
+		self.ptv3 = PointTransformerV3(
+			in_channels=6,
+			order=("z", "z-trans", "hilbert", "hilbert-trans"),
+			#enable_rpe=True,
+			#enable_flash=False,
+			stride=(2, 2, 2, 2),
+			enc_depths=(2, 2, 2, 6, 2),
+			enc_channels=(32, 64, 128, 256, 512),
+			enc_num_head=(2, 4, 8, 16, 32),
+			enc_patch_size=(128, 128, 128, 128, 128),
+			dec_depths=(2, 2, 2, 2),
+			dec_channels=(128, 128, 256, 256),
+			dec_num_head=(4, 4, 8, 16),
+			dec_patch_size=(128, 128, 128, 128),
+		)
+		
+		self.remove_mask = nn.Linear(128, 1)  
+		self.add_mask = nn.Linear(128, 1)  
+		
+	def forward(self, inlier_points, neighbour_points):
+
+		inlier_feats = inlier_points['feat'].unsqueeze(0).reshape(self.batch_size, self.num_points, -1)
+		inlier_coords = inlier_points['coord'].unsqueeze(0).reshape(self.batch_size, self.num_points, -1)
+		neighbour_feats = neighbour_points['feat'].unsqueeze(0).reshape(self.batch_size, self.num_points, -1)
+		neighbour_coords = neighbour_points['coord'].unsqueeze(0).reshape(self.batch_size, self.num_points, -1)
+		
+		all_points_feat = torch.cat([inlier_feats, neighbour_feats], dim=1).reshape(self.batch_size*self.num_points*2, -1)
+		all_points_coords = torch.cat([inlier_coords, neighbour_coords], dim=1).reshape(self.batch_size*self.num_points*2, -1)
+		all_points_offsets = torch.tensor([(i+1)*self.num_points*2 for i in range(self.batch_size)]).to("cuda")
+		all_points_grid_size = 0.01 # Customizable
+		all_points = {
+			'feat': all_points_feat,
+			'coord': all_points_coords,
+			'offset': all_points_offsets,
+			'grid_size': all_points_grid_size
+		}
+
+		x = self.ptv3(all_points)
+		features = x['feat'].reshape(self.batch_size, self.num_points*2, -1)
+		remove_feats = features[:, :self.num_points, :]
+		add_feats = features[:, self.num_points:, :]
+
+		remove_mask_logits = self.remove_mask(remove_feats).squeeze(-1)
+		add_mask_logits = self.add_mask(add_feats).squeeze(-1)
+
+		return remove_mask_logits, add_mask_logits
---
+++ train_region_grow_ptv3.py
@@ -0,0 +1,282 @@
+from ptv3_util import *
+import os
+import sys
+import time
+import numpy
+import h5py
+import torch
+import torch.nn as nn
+from model import *
+from tqdm import tqdm
+
+MODEL_PATH = None
+BATCH_SIZE = 32
+NUM_INLIER_POINT = 512
+NUM_NEIGHBOR_POINT = 512
+CURRENT_EPOCH = 0
+MAX_EPOCH = 64
+TRANSFORMER = False
+VAL_STEP = 7
+TRAIN_AREA = ['1']#,'2','3','scannet','4','5','6']
+VAL_AREA = None
+#	Set to 12 as curvatures may be nan in scan data.
+FEATURE_SIZE = 12
+MULTISEED = 0
+
+initialized = False
+cross_domain = False
+numpy.random.seed(0)
+numpy.set_printoptions(2,linewidth=100,suppress=True,sign=' ')
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f'Using device: {device}')
+
+net = RegionTransformerPTv3(BATCH_SIZE, 1, NUM_INLIER_POINT, NUM_NEIGHBOR_POINT, FEATURE_SIZE)
+net = net.to(device)
+torch.compile(net)
+
+optimizer = torch.optim.AdamW(net.parameters(), lr=1e-4)
+criterion = nn.BCEWithLogitsLoss()
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=16, eta_min=0)
+
+if CURRENT_EPOCH != 0:
+	checkpoint = torch.load("models/regiontransformer_model.pth")
+	net.load_state_dict(checkpoint['model_state_dict'])
+	optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+	print(f"Loaded checkpoint from epoch {CURRENT_EPOCH}")
+
+epoch_time = []
+
+for epoch in range(CURRENT_EPOCH, MAX_EPOCH):
+
+	if not initialized or MULTISEED > 1:
+		initialized = True
+		train_inlier_points, train_inlier_count, train_neighbor_points, train_neighbor_count, train_add, train_remove = [], [], [], [], [], []
+		val_inlier_points, val_inlier_count, val_neighbor_points, val_neighbor_count, val_add, val_remove = [], [], [], [], [], []
+
+		if VAL_AREA is not None and epoch % VAL_STEP == VAL_STEP - 1:
+			AREA_LIST = TRAIN_AREA + VAL_AREA
+		else:
+			AREA_LIST = TRAIN_AREA
+		for AREA in AREA_LIST:
+			if isinstance(AREA, str) and AREA.startswith('synthetic'):
+				f = h5py.File('data/staged_%s.h5' % AREA, 'r')
+			elif MULTISEED > 0 and AREA in TRAIN_AREA:
+				SEED = epoch % MULTISEED
+				try:
+					f = h5py.File('data/multiseed/seed%d_area%s.h5'%(SEED,AREA),'r')
+				except OSError:
+					print("OSError")
+					continue
+			else:
+				f = h5py.File('data/staged_area%s.h5'%(AREA),'r')
+			print('Loading %s ...'%f.filename)
+			if VAL_AREA is not None and AREA in VAL_AREA:
+				count = f['count'][:]
+				val_inlier_count.extend(count)
+				points = f['points'][:]
+				remove = f['remove'][:]
+				idp = 0
+				for i in range(len(count)):
+					val_inlier_points.append(points[idp:idp+count[i], :FEATURE_SIZE])
+					val_remove.append(remove[idp:idp+count[i]])
+					idp += count[i]
+				neighbor_count = f['neighbor_count'][:]
+				val_neighbor_count.extend(neighbor_count)
+				neighbor_points = f['neighbor_points'][:]
+				add = f['add'][:]
+				idp = 0
+				for i in range(len(neighbor_count)):
+					val_neighbor_points.append(neighbor_points[idp:idp+neighbor_count[i], :FEATURE_SIZE])
+					val_add.append(add[idp:idp+neighbor_count[i]])
+					idp += neighbor_count[i]
+			if AREA in TRAIN_AREA:
+				count = f['count'][:]
+				train_inlier_count.extend(count)
+				points = f['points'][:]
+				remove = f['remove'][:]
+				idp = 0
+				for i in range(len(count)):
+					train_inlier_points.append(points[idp:idp+count[i], :FEATURE_SIZE])
+					train_remove.append(remove[idp:idp+count[i]])
+					idp += count[i]
+				neighbor_count = f['neighbor_count'][:]
+				train_neighbor_count.extend(neighbor_count)
+				neighbor_points = f['neighbor_points'][:]
+				add = f['add'][:]
+				idp = 0
+				for i in range(len(neighbor_count)):
+					train_neighbor_points.append(neighbor_points[idp:idp+neighbor_count[i], :FEATURE_SIZE])
+					train_add.append(add[idp:idp+neighbor_count[i]])
+					idp += neighbor_count[i]
+			if FEATURE_SIZE is None:
+				FEATURE_SIZE = points.shape[1]
+			f.close()
+
+		#filter out instances where the neighbor array is empty
+		train_inlier_points = [train_inlier_points[i] for i in range(len(train_neighbor_count)) if train_neighbor_count[i]>0]
+		train_inlier_count = [train_inlier_count[i] for i in range(len(train_neighbor_count)) if train_neighbor_count[i]>0]
+		train_neighbor_points = [train_neighbor_points[i] for i in range(len(train_neighbor_count)) if train_neighbor_count[i]>0]
+		train_add = [train_add[i] for i in range(len(train_neighbor_count)) if train_neighbor_count[i]>0]
+		train_remove = [train_remove[i] for i in range(len(train_neighbor_count)) if train_neighbor_count[i]>0]
+		train_neighbor_count = [train_neighbor_count[i] for i in range(len(train_neighbor_count)) if train_neighbor_count[i]>0]
+		val_inlier_points = [val_inlier_points[i] for i in range(len(val_neighbor_count)) if val_neighbor_count[i]>0]
+		val_inlier_count = [val_inlier_count[i] for i in range(len(val_neighbor_count)) if val_neighbor_count[i]>0]
+		val_neighbor_points = [val_neighbor_points[i] for i in range(len(val_neighbor_count)) if val_neighbor_count[i]>0]
+		val_add = [val_add[i] for i in range(len(val_neighbor_count)) if val_neighbor_count[i]>0]
+		val_remove = [val_remove[i] for i in range(len(val_neighbor_count)) if val_neighbor_count[i]>0]
+		val_neighbor_count = [val_neighbor_count[i] for i in range(len(val_neighbor_count)) if val_neighbor_count[i]>0]
+		if len(train_inlier_points)==0:
+			continue
+		print('train',len(train_inlier_points),train_inlier_points[0].shape, len(train_neighbor_points))
+		print('val',len(val_inlier_points), len(val_neighbor_points))
+
+	idx = numpy.arange(len(train_inlier_points))
+	numpy.random.shuffle(idx)
+	inlier_points = numpy.zeros((BATCH_SIZE, NUM_INLIER_POINT, FEATURE_SIZE))
+	neighbor_points = numpy.zeros((BATCH_SIZE, NUM_NEIGHBOR_POINT, FEATURE_SIZE))
+	input_add = numpy.zeros((BATCH_SIZE, NUM_NEIGHBOR_POINT), dtype=numpy.int32)
+	input_remove = numpy.zeros((BATCH_SIZE, NUM_INLIER_POINT), dtype=numpy.int32)
+
+	loss_arr = []
+	add_prc_arr = []
+	add_rcl_arr = []
+	rmv_prc_arr = []
+	rmv_rcl_arr = []
+	num_batches = int(len(train_inlier_points) / BATCH_SIZE)
+	start_time = time.time()
+	net.train()
+	for batch_id in tqdm(range(num_batches)):
+		start_idx = batch_id * BATCH_SIZE
+		end_idx = (batch_id + 1) * BATCH_SIZE
+		for i in range(BATCH_SIZE):
+			points_idx = idx[start_idx+i]
+			N = train_inlier_count[points_idx]
+			if N >= NUM_INLIER_POINT:
+				subset = numpy.random.choice(N, NUM_INLIER_POINT, replace=False)
+			else:
+				subset = list(range(N)) + list(numpy.random.choice(N, NUM_INLIER_POINT-N, replace=True))
+			inlier_points[i,:,:] = train_inlier_points[points_idx][subset, :]
+			input_remove[i,:] = train_remove[points_idx][subset]
+			N = train_neighbor_count[points_idx]
+			if N >= NUM_NEIGHBOR_POINT:
+				subset = numpy.random.choice(N, NUM_NEIGHBOR_POINT, replace=False)
+			else:
+				subset = list(range(N)) + list(numpy.random.choice(N, NUM_NEIGHBOR_POINT-N, replace=True))
+			neighbor_points[i,:,:] = train_neighbor_points[points_idx][subset, :]
+			input_add[i,:] = train_add[points_idx][subset]
+
+		inlier_tensor = torch.FloatTensor(inlier_points).to(device)
+		inlier_normalized = inlier_tensor[:,:,3:6].reshape(-1, 3)
+		inlier_feats = inlier_tensor[:,:,6:FEATURE_SIZE].reshape(-1, FEATURE_SIZE-6)
+		inlier_offsets = torch.tensor([(i+1)*NUM_INLIER_POINT for i in range(BATCH_SIZE)]).to(device)
+		inlier_data = {
+				'coord': inlier_normalized,
+				'feat': inlier_feats,
+				'offset': inlier_offsets,
+				'grid_size': 0.01
+			}
+
+		neighbor_tensor = torch.FloatTensor(neighbor_points).to(device)
+		neighbor_coords = neighbor_tensor[:,:,:3].reshape(-1, 3)
+		neighbor_normalized = neighbor_tensor[:,:,3:6].reshape(-1, 3)
+		neighbor_feats = neighbor_tensor[:,:,6:FEATURE_SIZE].reshape(-1, FEATURE_SIZE-6)
+		neighbor_offsets = torch.tensor([(i+1)*NUM_NEIGHBOR_POINT for i in range(BATCH_SIZE)]).to(device)
+		neighbor_data = {
+			'coord': neighbor_normalized,
+			'feat': neighbor_feats,
+			'offset': neighbor_offsets,
+			'grid_size': 0.01
+		}			
+		add_mask_tensor = torch.FloatTensor(input_add).to(device)
+		remove_mask_tensor = torch.FloatTensor(input_remove).to(device)
+
+		optimizer.zero_grad()
+		remove_mask_logits, add_mask_logits = net(inlier_data, neighbor_data)
+
+		add_loss = criterion(add_mask_logits, add_mask_tensor)
+		remove_loss = criterion(remove_mask_logits, remove_mask_tensor)
+		loss = add_loss + remove_loss
+		loss.backward()
+		optimizer.step()
+
+		loss_arr.append(loss.item())
+		print("Batch %d Total_loss %.2f Loss %.2f"%(batch_id, numpy.mean(loss_arr), loss.item()))
+
+
+	epoch_time.append(time.time() - start_time)
+	# print("Epoch %d loss %.2f add %.2f/%.2f rmv %.2f/%.2f"%(epoch+1,numpy.mean(loss_arr),numpy.mean(add_prc_arr),numpy.mean(add_rcl_arr),numpy.mean(rmv_prc_arr), numpy.mean(rmv_rcl_arr)))
+	scheduler.step()
+
+	print("Epoch %d train loss %.2f"%(epoch+1,numpy.mean(loss_arr)))
+
+	if epoch % 1 == 0: # Save every 5 epochs
+		print(f"Saving checkpoint at epoch {epoch} with loss {numpy.mean(loss_arr):.4f}")
+		save_checkpoint(epoch, net, optimizer, loss, 'model_checkpoint.pth')
+
+	if VAL_AREA is not None and epoch % VAL_STEP == VAL_STEP - 1:
+		net.eval()
+		loss_arr = []
+		add_prc_arr = []
+		add_rcl_arr = []
+		rmv_prc_arr = []
+		rmv_rcl_arr = []
+		num_batches = int(len(val_inlier_points) / BATCH_SIZE)
+		for batch_id in tqdm(range(num_batches)):
+			start_idx = batch_id * BATCH_SIZE
+			end_idx = (batch_id + 1) * BATCH_SIZE
+			for i in range(BATCH_SIZE):
+				points_idx = start_idx+i
+				N = val_inlier_count[points_idx]
+				if N >= NUM_INLIER_POINT:
+					subset = numpy.random.choice(N, NUM_INLIER_POINT, replace=False)
+				else:
+					subset = list(range(N)) + list(numpy.random.choice(N, NUM_INLIER_POINT-N, replace=True))
+				inlier_points[i,:,:] = val_inlier_points[points_idx][subset, :]
+				input_remove[i,:] = val_remove[points_idx][subset]
+				N = val_neighbor_count[points_idx]
+				if N >= NUM_INLIER_POINT:
+					subset = numpy.random.choice(N, NUM_NEIGHBOR_POINT, replace=False)
+				else:
+					subset = list(range(N)) + list(numpy.random.choice(N, NUM_NEIGHBOR_POINT-N, replace=True))
+				neighbor_points[i,:,:] = val_neighbor_points[points_idx][subset, :]
+				input_add[i,:] = val_add[points_idx][subset]
+
+			inlier_tensor = torch.FloatTensor(inlier_points).to(device)
+			inlier_coords = inlier_tensor[:,:,:3].reshape(-1, 3)
+			inlier_normalized = inlier_tensor[:,:,3:6].reshape(-1, 3)
+			inlier_feats = inlier_tensor[:,:,6:FEATURE_SIZE].reshape(-1, FEATURE_SIZE-6)
+			inlier_offsets = torch.tensor([(i+1)*NUM_INLIER_POINT for i in range(BATCH_SIZE)]).to(device)
+			inlier_data = {
+						'coord': inlier_normalized,
+						'feat': inlier_feats,
+						'offset': inlier_offsets,
+						'grid_size': 0.01
+					}
+			
+			neighbor_tensor = torch.FloatTensor(neighbor_points).to(device)
+			neighbor_coords = neighbor_tensor[:,:,:3].reshape(-1, 3)
+			neighbor_normalized = neighbor_tensor[:,:,3:6].reshape(-1, 3)
+			neighbor_feats = neighbor_tensor[:,:,6:FEATURE_SIZE].reshape(-1, FEATURE_SIZE-6)
+			neighbor_offsets = torch.tensor([(i+1)*NUM_NEIGHBOR_POINT for i in range(BATCH_SIZE)]).to(device)
+			neighbor_data = {
+				'coord': neighbor_normalized,
+				'feat': neighbor_feats,
+				'offset': neighbor_offsets,
+				'grid_size': 0.01
+			}	
+			add_mask_tensor = torch.FloatTensor(input_add).to(device)
+			remove_mask_tensor = torch.FloatTensor(input_remove).to(device)
+			
+			with torch.no_grad():
+				remove_mask_logits, add_mask_logits = net(inlier_tensor, neighbor_tensor)
+				add_loss = criterion(add_mask_logits, add_mask_tensor)
+				remove_loss = criterion(remove_mask_logits, remove_mask_tensor)
+				ls = add_loss.item() + remove_loss.item()
+
+			loss_arr.append(ls)
+
+		print("Epoch %d validation loss %.2f"%(epoch+1,numpy.mean(loss_arr)))
+
+
+print("Avg Epoch Time: %.3f" % numpy.mean(epoch_time))
