0faa9289008a28b2f9b7bef04f645ca32c4ffcc7
Emad Alkhashab <emadelkhashab@hotmail.com>
2026-02-10 08:17:10 +0200

    Add util for picking points by open3d

--- pointcloud_utils.py
+++ pointcloud_utils.py
@@ -2,6 +2,20 @@ import os.path
 import open3d as o3d
 import numpy as np
 
+def pick_points(pcd):
+    print("")
+    print(
+        "1) Please pick at least three correspondences using [shift + left click]"
+    )
+    print("   Press [shift + right click] to undo point picking")
+    print("2) After picking points, press 'Q' to close the window")
+    vis = o3d.visualization.VisualizerWithEditing()
+    vis.create_window()
+    vis.add_geometry(pcd)
+    vis.run()  # user picks points
+    vis.destroy_window()
+    print("")
+    return vis.get_picked_points()
 
 def pts_to_ply(filename):
     ply_file = os.path.join(os.path.dirname(filename), os.path.basename(filename)[:-4]) + ".ply"
